771d517622a4434cb7af305b2fbd3d523c3c86b6
Ates Goral <ates@magnetiq.com>
2015-05-09 03:53:32 -0400

    Heat diffusion

---
+++ HeatField.js
@@ -0,0 +1,69 @@
+function HeatField(numCols, numRows) {
+	var HEAT_DIFFUSION_TIME_STEP = 10, // Milliseconds
+		HEAT_DIFFUSION_COEFF = 2 / 1000 * HEAT_DIFFUSION_TIME_STEP; // Per second
+
+	var HEAT_DIFFUSION_MATRIX = [
+	    [ 1/16, 1/8, 1/16 ], 
+	    [ 1/8, 1/4, 1/8 ], 
+	    [ 1/16, 1/8, 1/16 ]
+	];
+
+	var rows = Array.apply(null, new Array(numRows)).map(function () {
+		return Array.apply(null, new Array(numCols)).map(function () {
+			return 0;
+		});
+	});
+
+	for (var i = 0; i < 1000; i++) {
+		rows[Math.floor(Math.random() * (numRows - 2)) + 1][Math.floor(Math.random() * (numCols - 2)) + 1] = Math.random();
+	}
+
+	var buffer = Array.apply(null, new Array(numRows)).map(function () {
+		return Array.apply(null, new Array(numCols)).map(function () {
+			return 0;
+		});
+	});
+
+	var lastTime;
+
+	this.applyTime = function (currentTime) {
+		if (!lastTime) {
+			lastTime = currentTime;
+		}
+
+		var elapsed = currentTime - lastTime;
+
+		lastTime = currentTime;
+
+		if (elapsed > 0) {
+			do {
+				for (var rowIdx = 1; rowIdx < numRows - 1; rowIdx++) {
+					for (var colIdx = 1; colIdx < numCols - 1; colIdx++) {
+						var cellHeat = rows[rowIdx][colIdx],
+							averageHeat = 0;
+
+						for (var matRowIdx = 0; matRowIdx < 3; matRowIdx++) {
+							for (var matColIdx = 0; matColIdx < 3; matColIdx++) {
+								averageHeat +=
+									rows[rowIdx + matRowIdx - 1][colIdx + matColIdx - 1]
+									* HEAT_DIFFUSION_MATRIX[matRowIdx][matColIdx];
+							}
+						}
+
+						buffer[rowIdx][colIdx] = cellHeat + (averageHeat - cellHeat) * HEAT_DIFFUSION_COEFF;
+					}
+				}
+
+				elapsed -= HEAT_DIFFUSION_TIME_STEP;
+			} while (elapsed > 0);
+
+			var swap = rows;
+			rows = buffer;
+			buffer = swap;
+		}
+	};
+
+	this.getRows = function () {
+		return rows;
+	};
+}
---
+++ main.js
@@ -0,0 +1,44 @@
+var NUM_COLS = 32,
+	NUM_ROWS = 32,
+	TILE_SIZE = 16;
+
+window.onload = function () {
+	var heatField = new HeatField(NUM_COLS, NUM_ROWS);
+
+	var heatFieldCanvas = document.getElementById('heatField'),
+		heatFieldCtx = heatFieldCanvas.getContext('2d');
+
+	heatFieldCanvas.width = NUM_COLS * TILE_SIZE;
+	heatFieldCanvas.height = NUM_ROWS * TILE_SIZE;
+
+	heatFieldCtx.scale(TILE_SIZE, TILE_SIZE);
+
+	function compToHex(c) {
+		return Math.floor(c * 255 + 256).toString(16).substr(-2);
+	}
+	function rgbToHex(r, g, b) {
+		return '#' + compToHex(r) + compToHex(g) + compToHex(b);
+	}
+
+	function renderHeatField() {
+		heatField.getRows().forEach(function (row, rowIdx) {
+			row.forEach(function (heat, colIdx) {
+				heatFieldCtx.fillStyle = heat < 0.5
+					? rgbToHex(0, 0, (0.5 - heat) * 2)
+					: rgbToHex((heat - 0.5) * 2, 0, 0);
+
+				heatFieldCtx.fillRect(colIdx, rowIdx, 1, 1);
+			});
+		});
+	}
+
+	function render(currentTime) {
+		heatField.applyTime(currentTime);
+
+		renderHeatField();
+
+		requestAnimationFrame(render);
+	}
+
+	window.requestAnimationFrame(render);
+};
